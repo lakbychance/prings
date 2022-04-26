c1f5e123177270e7c719185d71e46cfdc79f9731
Lakshya Thakur <lapstjup@gmail.com>
2022-04-27 01:35:07 +0530

    fix: move `title` tag from _app to _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,13 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Prings</title>
+      </Head>
+      <Component {...pageProps} />
+    </>)
 }
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,8 +23,6 @@ const Document = () => {
                 />
                 <link rel="manifest" href="/manifest.json" />
 
-                <title>Prings</title>
-
                 <meta charSet="utf-8" />
                 <meta name="msapplication-config" content="/browserconfig.xml" />
                 <meta name="msapplication-TileColor" content="	#00800" />
@@ -33,6 +31,7 @@ const Document = () => {
                 <meta name="title" content="Prings" />
                 <meta name="description" content="Add LinkedIn style profile rings to your images" />
 
+                {/*<!-- Open Graph / Facebook -->*/}
                 <meta property="og:title" content="Prings" />
                 <meta
                     property="og:description"
@@ -45,6 +44,7 @@ const Document = () => {
                     content="https://ik.imagekit.io/lapstjup/prings/prings_og_image.png"
                 />
 
+                {/* <!-- Twitter -->*/}
                 <meta property="twitter:card" content="summary_large_image" />
                 <meta property="twitter:url" content="https://www.prings.vercel.app" />
                 <meta property="twitter:title" content="Prings" />
